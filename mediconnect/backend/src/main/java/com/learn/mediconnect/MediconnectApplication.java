9d9ec4edfc1fedb3182e64ad714f1028865393a3
Md Selim <whiteblueskyss@gmail.com>
2025-10-09 19:22:47 +0600

    backend Spring boot project set up

---
+++ mediconnect/backend/src/main/java/com/learn/mediconnect/MediconnectApplication.java
@@ -0,0 +1,13 @@
+package com.learn.mediconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediconnectApplication.class, args);
+	}
+
+}
---
+++ mediconnect/backend/src/test/java/com/learn/mediconnect/MediconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.mediconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
